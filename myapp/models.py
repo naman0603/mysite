d37d4999cff8bbe31d20d5f35a8f9330018de1da
Naman Shah <103444680+naman0603@users.noreply.github.com>
2025-02-03 11:19:57 -0500

    Initial commit

---
+++ myapp/models.py
@@ -0,0 +1,61 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    warehouse = models.CharField(max_length=100, default="Default Warehouse")
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=100)
+    available = models.BooleanField(default=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Client(User):
+    PROVINCE_CHOICES = [
+        ('AB', 'Alberta'),
+        ('MB', 'Manitoba'),
+        ('ON', 'Ontario'),
+        ('QC', 'Quebec'),
+    ]
+
+    company = models.CharField(max_length=50, null=True, blank=True)
+    shipping_address = models.CharField(max_length=300, null=True, blank=True)
+    city = models.CharField(max_length=20, default='Windsor')
+    province = models.CharField(max_length=2, choices=PROVINCE_CHOICES, default='ON')
+    interested_in = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.username
+
+
+class Order(models.Model):  # Move this out of the Client class
+    ORDER_STATUS_CHOICES = [
+        (0, 'Order Cancelled'),
+        (1, 'Order Placed'),
+        (2, 'Order Shipped'),
+        (3, 'Order Delivered'),
+    ]
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    num_units = models.PositiveIntegerField()
+    order_status = models.PositiveIntegerField(choices=ORDER_STATUS_CHOICES, default=1)
+    order_date = models.DateField()
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.product.name}"
+
+    def total_cost(self):
+        return self.product.price * self.num_units
